e8b69a8a554532253ac4de181ad57cd22eb1129b
Nick <Nelo-001@mail.ru>
2020-09-26 18:59:24 +0300

    web pack

--- task_api/server/app.js
+++ task_api/server/app.js
@@ -1,13 +1,41 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("../schema/schema");
+const {graphqlHTTP} = require('express-graphql');
 const cors = require("cors");
+const { buildSchema } = require("graphql");
+const { readFileSync } = require("fs");
+
+const schemaString = readFileSync("server/schema.graphql", {
+  encoding: "utf-8",
+});
+const schema = buildSchema(schemaString);
+
+const list = [
+  { id: 0, title: "Some Title", text: "Some Text" },
+  { id: 1, title: "Some Title", text: "Some Text" },
+  { id: 2, title: "Some Title", text: "Some Text" },
+  { id: 3, title: "Some Title", text: "Some Text" },
+  { id: 4, title: "Some Title", text: "Some Text" },
+  { id: 5, title: "Some Title", text: "Some Text" },
+  { id: 6, title: "Some Title", text: "Some Text" },
+  { id: 7, title: "Some Title", text: "Some Text" },
+  { id: 8, title: "Some Title", text: "Some Text" },
+  { id: 9, title: "Some Title", text: "Some Text" },
+];
+
+const root = {
+  getAllList: () => {
+    return list;
+  },
+  getListById: (params) => {
+    return list.find(x=>x.id == params.id)
+  }
+};
+
+//-----------------------------------------------------------------------
 
 const app = express();
 app.use(cors());
-app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
-
-
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true, rootValue: root }));
 
 app.listen(3005, (err) => {
   err ? console.error(`Error  ${err}`) : console.log("Server started");
